708bc47b8d956569c1b57d25f40cdb67bc4aed4f
Daniel Wood <109208162+danwood1992@users.noreply.github.com>
2023-04-05 12:15:00 +0100

    Update storage-tool.ps1

--- storage-tool.ps1
+++ storage-tool.ps1
@@ -1,6 +1,14 @@
-echo "Storage tool for windows server "
-echo "================================"
-echo "Current disks on server"
+# Define the minimum and maximum sizes for the volume
+$volumeSizeMin = 1 # GB
+$volumeSizeMax = 1024 # TB
+
+# Define the valid parity options
+$validParities = "simple", "mirror", "parity"
+
+# Display a header
+Write-Host "Storage tool for Windows Server"
+Write-Host "================================"
+
 # Display all current disks on the server
 Get-Disk | Format-Table
 
@@ -20,11 +28,18 @@ if ($offlineDisks.Count -eq 0) {
         $diskNumber = $_.Trim()
         $disk = Get-Disk -Number $diskNumber
         $diskName = Read-Host "Enter a name for disk $diskNumber:"
-        $size = Read-Host "Enter the size of the volume you want to create on disk $diskNumber (in GB):"
-        $size = [int]$size
-        $parity = Read-Host "Enter the parity for the volume you want to create on disk $diskNumber (simple/mirror/parity):"
+        $size = Read-Host "Enter the size of the volume you want to create on disk $diskNumber (in GB, minimum $volumeSizeMin, maximum $volumeSizeMax):"
+        while ($size -lt $volumeSizeMin -or $size -gt $volumeSizeMax) {
+            Write-Warning "Invalid volume size. Please enter a value between $volumeSizeMin and $volumeSizeMax GB."
+            $size = Read-Host "Enter the size of the volume you want to create on disk $diskNumber (in GB, minimum $volumeSizeMin, maximum $volumeSizeMax):"
+        }
+        $parity = Read-Host "Enter the parity for the volume you want to create on disk $diskNumber (valid options: simple/mirror/parity):"
+        while (-not $validParities.Contains($parity)) {
+            Write-Warning "Invalid parity option. Please enter a valid option (simple/mirror/parity)."
+            $parity = Read-Host "Enter the parity for the volume you want to create on disk $diskNumber (valid options: simple/mirror/parity):"
+        }
         $volumeName = Read-Host "Enter a name for the volume you want to create on disk $diskNumber:"
-        New-Volume -FriendlyName $volumeName -StoragePoolFriendlyName "StoragePool01" -PhysicalDisks $disk -FileSystem CSVFS_ReFS -ResiliencySettingName $parity -Size $size
+        New-Volume -FriendlyName $volumeName -StoragePoolFriendlyName "StoragePool01" -PhysicalDisks $disk -FileSystem CSVFS_ReFS -ResiliencySettingName $parity -Size $size"GB"
     }
 
     # Bring the selected disks online
@@ -38,8 +53,3 @@ if ($offlineDisks.Count -eq 0) {
     Write-Host "`nNew volumes:`n"
     Get-Volume $volumes | Format-Table
 }
-
-
-
-
-
